db44fa0f2e6877b3f2622edf560441edaaffedaa
bavrahami00 <bavrahami00@stuy.edu>
2019-02-12 17:58:40 -0500

    toString

--- KnightBoard.java
+++ KnightBoard.java
@@ -3,4 +3,23 @@ public class KnightBoard {
   public KnightBoard(int rows, int cols) {
     data = new int[rows][cols];
   }
+  public String toString() {
+    String ans = "";
+    for (int x = 0; x < board.length; x++) {
+      for (int i = 0; i < board[0].length-1; i++) {
+        if (board[x][i] < 10) {
+          ans += " ";
+         }
+         ans += board[x][i] + " ";
+      }
+      if (board[x][board[x].length-1] < 10) {
+        ans += " ";
+      }
+      ans += board[x][board[x].length];
+      if (x != board.length-1) {
+        ans += "\n";
+      }
+    }
+    return ans;
+  }
 }
